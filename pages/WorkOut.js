7f32177c65f78ed77502623723815a5fa16a2093
Jonssoni <n2nejo00@students.oamk.fi>
2024-08-29 19:21:03 +0300

    some testing with workout buttons

--- pages/WorkOut.js
+++ pages/WorkOut.js
@@ -1,26 +1,46 @@
-
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const WorkOut = () => {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>WorkOut Screen</Text>
+      <TouchableOpacity style={styles.button} onPress={() => alert('Button 1 pressed')}>
+        <Text style={styles.buttonText}>Button 1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => alert('Button 2 pressed')}>
+        <Text style={styles.buttonText}>Button 2</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => alert('Button 3 pressed')}>
+        <Text style={styles.buttonText}>Button 3</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
+    flexDirection: 'row', 
+    justifyContent: 'space-between',  
+    padding: 15, 
+    borderWidth:1,
+    margin:10,
+    borderRadius:20
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  button: {
+    flex: 1,  
+    backgroundColor: '#c6cfda',  
+    paddingVertical: 20,  
+    marginHorizontal: 5,  
+    borderRadius: 8,  
+    alignItems: 'center',  
+    
+  
+  },
+  buttonText: {
+    color: '#FFFFFF',  
   },
 });
 
-export default WorkOut;
+export default App;
